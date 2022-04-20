1e996624dd9620ff2b6ee2afda15c9eec7ffc57f
Anggraeni Xena <anggraexena@gmail.com>
2022-04-20 10:24:35 +0700

    Tambah input select registrasi berdasarkan level ref #6

--- resources/views/registrasi.blade.php
+++ resources/views/registrasi.blade.php
@@ -3,6 +3,19 @@
     <div style="margin-left: 15% ; margin-right: 15%; margin-top:5%">
         <form class="row g-3">
             <div class="col-md-6">
+                <label for="inputFirstName" class="form-label">Registrasi sebagai</label>
+                <select class="form-select" aria-label="Default select example" style="background-color: #A3B18A">
+                    <option selected disabled>Open this select menu</option>
+                    <option value="1">Perusahaan</option>
+                    <option value="2">Pekerja</option>
+                    <option value="3">Admin</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="inputUsername" class="form-label">Username</label>
+                <input type="text" class="form-control" id="inputUsername" style="background-color: #A3B18A">
+            </div>
+            <div class="col-md-6">
                 <label for="inputFirstName" class="form-label">First Name</label>
                 <input type="email" class="form-control" id="inputFirstName" style="background-color: #A3B18A">
             </div>
@@ -13,10 +26,6 @@
             <div class="col-12">
                 <label for="inputAddress" class="form-label">Email Address</label>
                 <input type="text" class="form-control" id="inputAddress" style="background-color: #A3B18A">
-            </div>
-            <div class="col-12">
-                <label for="inputUsername" class="form-label">Username</label>
-                <input type="text" class="form-control" id="inputUsername" style="background-color: #A3B18A">
             </div>
             <div class="col-12">
                 <label for="inputPassword" class="form-label">Password</label>
